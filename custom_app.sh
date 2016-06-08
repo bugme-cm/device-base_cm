6c9e42192993abf16202717e4f6b303266e97df8
moonlightly <906268147@qq.com>
2016-06-08 20:57:00 +0800

    Revert "Use NianUpdater as default Updater"
    This reverts commit 8f37d80175402805bab5649f0da77e99834299d9.

--- custom_app.sh
+++
@@ -1,9 +0,0 @@
-apkBaseName=$1
-tempSmaliDir=$2
-dir=$(cd "$(dirname "$0")"; pwd)
-
-
-if [ $1 = "Settings" ];then
-	echo ">>> in custom_app $apkBaseName"
-	cp $dir/$1/res/xml/mz_device_info_settings.xml $tempSmaliDir/res/xml/mz_device_info_settings.xml
-fi
